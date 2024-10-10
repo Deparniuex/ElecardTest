b513ae56a2550d285c552f22a34c00a5a88bd272
Deparniuexx <dep2and@gmail.com>
2024-10-10 14:01:04 +0700

    setted up config

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"github.com/spf13/viper"
+)
 
+func main() {
+	err := setupConfig("/config/.env")
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func setupConfig(path string) error {
+	viper.SetConfigName(path)
+	return viper.ReadInConfig()
 }
